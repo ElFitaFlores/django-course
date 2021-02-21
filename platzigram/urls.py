f2efba7dad52a9881e8416c675717975ea3fae55
Rafael Flores <fita500@gmail.com>
2021-02-20 21:31:09 -0600

    Add posts app and list posts view

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from platzigram.views import hello_world, hi, order_numbers, say_hi
+from platzigram import views as local_views
+from posts import views as posts_views
 
 urlpatterns = [
-    path('hello-world/', hello_world),
-    path('hi/', hi),
-    path('numbers/', order_numbers),
-    path('hi/<str:name>/<int:age>/', say_hi),
+    path('hello-world/', local_views.hello_world),
+    path('hi/', local_views.hi),
+    path('numbers/', local_views.order_numbers),
+    path('hi/<str:name>/<int:age>/', local_views.say_hi),
+    path('posts', posts_views.list_posts)
 ]
